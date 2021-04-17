ce468033db881de665e2d598d1877e0692c4984e
Evgeny Kolyakov <freaker2k7@users.noreply.github.com>
2021-04-17 09:09:19 +0300

    Added timing stop & start mechanism
    Following https://stackoverflow.com/questions/67090593/how-to-pause-pytest-timer I decided to add that functionality.

--- src/_pytest/timing.py
+++ src/_pytest/timing.py
@@ -7,6 +7,45 @@ Fixture "mock_timing" also interacts with this module for pytest's own tests.
 """
 from time import perf_counter
 from time import sleep
-from time import time
+from time import time as Time
 
-__all__ = ["perf_counter", "sleep", "time"]
+
+PAUSED_FOR = 0
+PAUSE_START = 0
+
+
+def stop():
+  """Pause the timing.
+  
+  Pracrically, we start a measurement when the pause started
+  so we can substract the diff from the final time.
+  
+  NOTE: Please don't use this with paralellized tests!
+  """
+  global PAUSE_START
+  
+  if PAUSE_START:
+    raise Exception("Maybe you meant to call pytest.timing.start() ?")
+  
+  PAUSE_START = Time()
+
+def start():
+  """Continue the original timing.
+  
+  Here, we count how long did we pause for and reset the pasue start.
+  
+  NOTE: Please don't use this with paralellized tests!
+  """
+  global PAUSED_FOR, PAUSE_START
+  
+  if not PAUSE_START:
+    raise Exception("Maybe you meant to call pytest.timing.stop() ?")
+  
+  PAUSED_FOR += Time() - PAUSE_START
+  PAUSE_START = 0
+
+def time():
+  return Time() - PAUSED_FOR
+
+
+__all__ = ["perf_counter", "sleep", "time", "start", "stop"]
